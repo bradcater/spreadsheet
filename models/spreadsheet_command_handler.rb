29caf1ed9f2fcf7cf3b26c0ffe1c278889bc781c
Brad Cater <bradcater@bradcater-Aspire-5750>
2014-12-11 08:21:42 -0500

    Fixed a bug in which GET did not respond to Excel-style (A0) addressing.

--- models/spreadsheet_command_handler.rb
+++ models/spreadsheet_command_handler.rb
@@ -9,8 +9,9 @@ class SpreadsheetCommandHandler
   end
   def spreadsheet ; @spreadsheet ; end
 
+  VALID_COMMANDS = Set.new(%w{ADDROW ADDCOLUMN GET SET})
   def handles_command?(cmd)
-    %w{ADDROW ADDCOLUMN GET SET}.include?(cmd[0])
+    VALID_COMMANDS.include?(cmd[0])
   end
 
   def handle_command(cmd)
@@ -19,22 +20,26 @@ class SpreadsheetCommandHandler
       @spreadsheet.add_row
     elsif cmd[0] == 'ADDCOLUMN'
       @spreadsheet.add_column
-    elsif cmd[0] == 'GET'
-      puts @spreadsheet.get_cell(cmd[1].to_i, cmd[2].to_i)
-    elsif cmd[0] == 'SET'
+    elsif %{GET SET}.include?(cmd[0])
       if cmd[1] =~ /^[A-Z]+[0-9]+$/
-        value = cmd[2..-1].join(' ')
+        value = cmd[2..-1].join(' ') if cmd[0] == 'SET'
         row_idx = cmd[1].match(/([0-9]+$)/)[0].to_i
         col_idx = Formatter.column_label_to_index(cmd[1].match(/(^[A-Z]+)/)[0])
       else
-        value = cmd[3..-1].join(' ')
+        value = cmd[3..-1].join(' ') if cmd[0] == 'SET'
         row_idx, col_idx = if cmd[1] =~ /^[A-Z]+$/
           [cmd[2].to_i, Formatter.column_label_to_index(cmd[1])]
         else
           [cmd[1].to_i, cmd[2].to_i]
         end
       end
-      @spreadsheet.set_cell(row_idx, col_idx, value)
+      if cmd[0] == 'GET'
+        puts @spreadsheet.get_cell(row_idx, col_idx)
+      elsif cmd[0] == 'SET'
+        @spreadsheet.set_cell(row_idx, col_idx, value)
+      else
+        raise "Unexpected cmd[0]: #{cmd[0]}"
+      end
     else
       raise "Unhandled cmd: #{cmd}"
     end
